0d1df49fefcd510823ced2bce5419122f065b167
Fenirr <162t0305@itsm.edu.mx>
2017-02-24 12:07:06 -0600

    con retorno y con parametro
    ejemplo de método

---
+++ ejemplo4.2.java
@@ -0,0 +1,17 @@
+package metodo4;
+
+public class ejemplo2 {
+public void main (String []args){
+	int resul;
+	resul= suma(6,8);
+	System.out.println("la sumar es:"+resul);
+	
+	
+}
+public static int suma(int c,int n){
+	int suma= c+n;
+	 
+	return suma;
+	
+}
+}
